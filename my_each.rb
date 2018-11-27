1d066a656fbf99e4ef8e970cca4a81681bdd76f4
Adam Dudley <dudleyadam@gmail.com>
2018-11-27 23:04:45 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,10 @@
 def my_each(array)
   i = 0
 
-  my_arrays = [ ]
-  split_array = array.split(" ")
-
   while i < array.length
     yield(array)
     i = i + 1
   end
 
-  array.join
+  arrays
 end
